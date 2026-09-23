961c686ac65231a27893bf23f2001f867d66759e
diegoserrano0103 <wilson65667@gmail.com>
2026-09-22 22:16:57 -0600

    Update main.cpp
    Hice cambios en la estructura del texto, quite el IF y corregí errores de codigo por palabras diferentes como std::cout y había agragado una n

--- main.cpp
+++ main.cpp
@@ -9,16 +9,17 @@ int main() {
     // 2. Variables (siempre inicializadas)
     double suma   = 0.0;
     double numero = 0.0;
+    int contador=0; 
 
     std::cout << "Suma de " << CANTIDAD << " numeros\n";
 
     // 3. Ciclo: repetir CANTIDAD veces
     //    TODO: elige un ciclo (for o while) y usa un contador
     //    TODO: pedir el numero, leerlo y acumularlo en suma
-    while (contador < 5){
-        std::count << "Ingresa Un Número"; 
+    while (contador < CANTIDAD){
+        std::cout << "Ingresa Un Número"; 
         std::cin >> numero;
-        if (numero >=0)
+        
         suma+=numero; 
         contador++;
     }
